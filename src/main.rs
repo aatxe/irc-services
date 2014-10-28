553fd04c947b0e46472f6a643e90b5ab0cc57a1b
Aaron Weiss <aaronweiss74@gmail.com>
2014-10-28 14:33:35 -0400

    Added basic bot stub.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 extern crate irc;
 
+use irc::Bot;
+use irc::bot::IrcBot;
+
 fn main() {
-    println!("Hello, world!")
+    let mut bot = IrcBot::new(|_, _, _, _| {
+        Ok(())
+    }).unwrap();
+    bot.identify().unwrap();
+    bot.output().unwrap();
 }
