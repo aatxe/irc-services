67d79a5a42cdaa5ff86409e193672b73df34a97e
Aaron Weiss <aaronweiss74@gmail.com>
2014-10-28 17:31:06 -0400

    Simplified existing layout some.

--- src/func/mod.rs
+++ src/func/mod.rs
@@ -8,18 +8,15 @@ use irc::data::{IrcReader, IrcWriter};
 mod nickserv;
 
 pub fn process<T, U>(bot: &IrcBot<T, U>, source: &str, command: &str, args: &[&str]) -> IoResult<()> where T: IrcWriter, U: IrcReader {
-    match (command, args) {
-        ("PRIVMSG", [chan, msg]) => {
-            if chan.starts_with("#") { return Ok(()); }
-            let user = source.find('!').map_or("", |i| source[..i]);
-            let tokens: Vec<_> = msg.split_str(" ").collect();
-            match tokens[0] {
-                "register" => (),
-                "identify" => (),
-                _ => (),
-            }
-        },
-        _ => (),
+    if let ("PRIVMSG", [chan, msg]) = (command, args) {
+        if chan.starts_with("#") { return Ok(()); }
+        let user = source.find('!').map_or("", |i| source[..i]);
+        let tokens: Vec<_> = msg.split_str(" ").collect();
+        match tokens[0] {
+            "register" => (),
+            "identify" => (),
+            _ => (),
+        }
     }
     Ok(())
 }
