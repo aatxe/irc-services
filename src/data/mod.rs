6f8aabc086bc769abed6d9f9b75c68bac00ba03d
Aaron Weiss <aaronweiss74@gmail.com>
2014-10-29 12:14:51 -0400

    Moved password_hash to data module.

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -1,3 +1,14 @@
+use std::io::IoResult;
+use crypto::sbuf::StdHeapAllocator;
+use crypto::sha3::{hash, Sha3_512};
+use serialize::hex::ToHex;
+
 pub mod user;
 
 pub type BotResult<T> = Result<T, String>;
+
+pub fn password_hash(password: &str) -> IoResult<String> {
+    let mut data = [0u8, ..64];
+    try!(hash::<StdHeapAllocator>(Sha3_512, password.as_bytes(), data));
+    Ok(data.to_hex())
+}
--- src/data/user.rs
+++ src/data/user.rs
@@ -1,8 +1,6 @@
+use super::password_hash;
 use std::io::fs::{File, PathExtensions, mkdir_recursive};
 use std::io::{FilePermission, InvalidInput, IoError, IoResult};
-use crypto::sbuf::StdHeapAllocator;
-use crypto::sha3::{hash, Sha3_512};
-use serialize::hex::ToHex;
 use serialize::json::{decode, encode};
 
 #[deriving(Encodable, Decodable, Show, PartialEq)]
@@ -16,13 +14,13 @@ impl User {
     pub fn new(nickname: &str, password: &str, email: Option<&str>) -> IoResult<User> {
         Ok(User {
             nickname: nickname.into_string(),
-            password: try!(User::password_hash(password)),
+            password: try!(password_hash(password)),
             email: email.map(|s| s.into_string()),
         })
     }
 
     pub fn is_password(&self, password: &str) -> IoResult<bool> {
-        Ok(self.password == try!(User::password_hash(password)))
+        Ok(self.password == try!(password_hash(password)))
     }
 
     pub fn exists(nickname: &str) -> bool {
@@ -50,16 +48,11 @@ impl User {
         let mut f = File::create(&Path::new(path[]));
         f.write_str(encode(self)[])
     }
-
-    pub fn password_hash(password: &str) -> IoResult<String> {
-        let mut data = [0u8, ..64];
-        try!(hash::<StdHeapAllocator>(Sha3_512, password.as_bytes(), data));
-        Ok(data.to_hex())
-    }
 }
 
 #[cfg(test)]
 mod test {
+    use super::super::password_hash;
     use super::User;
     use std::io::fs::unlink;
 
@@ -67,12 +60,12 @@ mod test {
     fn new() {
         assert_eq!(User::new("test", "test", None).unwrap(), User {
             nickname: "test".into_string(),
-            password: User::password_hash("test").unwrap(),
+            password: password_hash("test").unwrap(),
             email: None,
         });
         assert_eq!(User::new("test", "test", Some("test@test.com")).unwrap(), User {
             nickname: "test".into_string(),
-            password: User::password_hash("test").unwrap(),
+            password: password_hash("test").unwrap(),
             email: Some("test@test.com".into_string()),
         });
     }
@@ -98,10 +91,6 @@ mod test {
         assert!(u.save().is_ok());
         let v = User::load("test3");
         assert!(v.is_ok());
-        assert_eq!(v.unwrap(), User {
-            nickname: "test3".into_string(),
-            password: User::password_hash("test").unwrap(),
-            email: None,
-        });
+        assert_eq!(v.unwrap(), u);
     }
 }
