f01b6d21084634df505fcf1a82983c2a9108f1c8
Aaron Weiss <aaronweiss74@gmail.com>
2014-12-11 14:52:25 -0500

    Added unit tests for Democracy.

--- src/data/democracy.rs
+++ src/data/democracy.rs
@@ -8,6 +8,7 @@ use irc::data::kinds::{IrcReader, IrcWriter};
 use irc::server::Server;
 use irc::server::utils::Wrapper;
 
+#[deriving(PartialEq, Show)]
 pub struct Democracy {
     proposals: HashMap<u8, Proposal>,
     votes: HashMap<String, Vec<Vote>>,
@@ -84,7 +85,7 @@ impl Democracy {
         let mut ret = Vec::new();
         for (id, proposal)  in self.proposals.iter() {
             let (yea, nay) = self.get_vote_counts(*id).unwrap();
-            ret.push(format!("Proposal ({}) to {} (Y: {}, N {}).", id, proposal.to_string(), yea, 
+            ret.push(format!("Proposal ({}) to {} (Y: {}, N {}).", id, proposal.display(), yea, 
                              nay));
         }
         ret
@@ -128,12 +129,14 @@ impl Democracy {
     }
 }
 
+#[deriving(PartialEq, Show)]
 pub enum VotingResult {
     VoteIssued,
     InvalidVote,
     NoSuchProposal,
 }
 
+#[deriving(PartialEq, Show)]
 pub enum VoteResult {
     VotePassed(Proposal),
     VoteFailed,
@@ -141,6 +144,7 @@ pub enum VoteResult {
     NoSuchVote,
 }
 
+#[deriving(PartialEq, Show)]
 enum Vote {
     Yea(u8),
     Nay(u8)
@@ -163,6 +167,7 @@ impl Vote {
     }
 }
 
+#[deriving(PartialEq, Show)]
 enum Proposal {
     ChangeOwner(String),
     Oper(String),
@@ -175,8 +180,8 @@ enum Proposal {
     Mode(String),
 }
 
-impl ToString for Proposal {
-    fn to_string(&self) -> String {
+impl Proposal {
+    fn display(&self) -> String {
         format!("{}", match self {
             &Proposal::ChangeOwner(ref owner) => format!("change the owner to {}", owner[]),
             &Proposal::Oper(ref user) => format!("oper {}", user[]),
@@ -186,10 +191,8 @@ impl ToString for Proposal {
             &Proposal::Mode(ref mode) => format!("change the channel mode to {}", mode[]),
         })
     }
-}
-
-impl Proposal {
-     pub fn from_str(proposal: &str, parameter: &str) -> Option<Proposal> {
+        
+    pub fn from_str(proposal: &str, parameter: &str) -> Option<Proposal> {
         let param = parameter.into_string();
         match proposal {
             "chown" => Some(Proposal::ChangeOwner(param)),
@@ -258,5 +261,74 @@ impl<'a, T: IrcReader, U: IrcWriter> Proposal {
             server.send_privmsg(chan, 
                                 format!("Failed to enact proposal to {}.", self.to_string())[])
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::{Democracy, VoteResult, VotingResult};
+    use std::collections::HashMap;
+
+    #[test]
+    fn new() {
+        assert_eq!(Democracy::new(), Democracy {
+            proposals: HashMap::new(), 
+            votes: HashMap::new(),
+            last_proposal: 0 
+        });   
+    }
+
+    #[test]
+    fn propose() {
+        let mut dem = Democracy::new();
+        assert_eq!(dem.propose("chown", "test"), Some(0));
+        assert_eq!(dem.propose("oper", "test"), Some(1));        
+        assert_eq!(dem.propose("deop", "test"), Some(2));
+        assert_eq!(dem.propose("kick", "test"), Some(3));
+        assert_eq!(dem.propose("mode", "+i"), Some(4));
+    }
+
+    #[test]
+    fn vote() {
+        let mut dem = Democracy::new();
+        assert_eq!(dem.propose("oper", "test"), Some(0));
+        assert!(!dem.has_voted(0, "test"));
+        assert_eq!(dem.vote(0, "yea", "test"), VotingResult::VoteIssued);
+        assert!(dem.has_voted(0, "test"));
+        assert_eq!(dem.vote(1, "yea", "test"), VotingResult::NoSuchProposal);
+        assert_eq!(dem.vote(0, "test", "test"), VotingResult::InvalidVote);
+    }
+
+    #[test]
+    fn get_result_of_vote() {
+        let mut dem = Democracy::new();
+        assert_eq!(dem.get_result_of_vote(0, 1), VoteResult::NoSuchVote);
+        assert_eq!(dem.propose("oper", "test"), Some(0));
+        assert_eq!(dem.get_result_of_vote(0, 1), VoteResult::VoteInProgress);
+        assert_eq!(dem.vote(0, "yea", "test"), VotingResult::VoteIssued);
+        assert!(match dem.get_result_of_vote(0, 1) { 
+            VoteResult::VotePassed(_) => true,
+            _ => false,
+        });
+        assert_eq!(dem.propose("oper", "test"), Some(0));
+        assert_eq!(dem.get_result_of_vote(0, 1), VoteResult::VoteInProgress);
+        assert_eq!(dem.vote(0, "nay", "test"), VotingResult::VoteIssued);
+        assert_eq!(dem.get_result_of_vote(0, 1), VoteResult::VoteFailed);
+    }
+
+    #[test]
+    fn is_full_vote() {
+        let mut dem = Democracy::new();
+        assert_eq!(dem.propose("chown", "test"), Some(0));
+        assert_eq!(dem.propose("oper", "test"), Some(1));        
+        assert_eq!(dem.propose("deop", "test"), Some(2));
+        assert_eq!(dem.propose("kick", "test"), Some(3));
+        assert_eq!(dem.propose("mode", "+i"), Some(4));
+        assert_eq!(dem.is_full_vote(0), true);
+        assert_eq!(dem.is_full_vote(1), true);
+        assert_eq!(dem.is_full_vote(2), true);
+        assert_eq!(dem.is_full_vote(3), false);
+        assert_eq!(dem.is_full_vote(4), false);
+        assert_eq!(dem.is_full_vote(5), false); // it's not a full vote if it doesn't exist.
     }
 }
