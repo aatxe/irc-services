cf43d7b31692ba34bac464c13d0e93809df0af19
Aaron Weiss <aaronweiss74@gmail.com>
2014-12-14 19:38:43 -0500

    Changed from crypto.rs to OpenSSL.

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -1,7 +1,6 @@
 use std::io::IoResult;
-#[cfg(not(test))] use crypto::sbuf::StdHeapAllocator;
-#[cfg(not(test))] use crypto::sha3::{hash, Sha3Mode};
-#[cfg(not(test))] use serialize::hex::ToHex;
+use openssl::crypto::hash::{HashType, Hasher};
+use serialize::hex::ToHex;
 
 pub mod channel;
 #[cfg(feature = "democracy")] pub mod democracy;
@@ -12,14 +11,8 @@ pub mod user;
 
 pub type BotResult<T> = Result<T, String>;
 
-#[cfg(not(test))]
 pub fn password_hash(password: &str) -> IoResult<String> {
-    let mut data = [0u8, ..64];
-    try!(hash::<StdHeapAllocator>(Sha3Mode::Sha3_512, password.as_bytes(), &mut data));
-    Ok(data.to_hex())
-}
-
-#[cfg(test)]
-pub fn password_hash(password: &str) -> IoResult<String> {
-    Ok(password.into_string())
+    let mut hasher = Hasher::new(HashType::SHA512);
+    try!(hasher.write_str(password));
+    Ok(hasher.finalize().to_hex())
 }
