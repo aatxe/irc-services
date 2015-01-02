17669119eea748c504eb0322976965ee9947601d
Aaron Weiss <aaronweiss74@gmail.com>
2015-01-01 21:10:07 -0500

    Fixed test compilation.

--- src/data/state.rs
+++ src/data/state.rs
@@ -60,7 +60,7 @@ impl State {
 
     #[cfg(test)]
     pub fn no_users_identified(&self) -> bool {
-        self.identified.lock().is_empty()
+        self.identified.lock().unwrap().is_empty()
     }
 
     #[cfg(feature = "resistance")]
